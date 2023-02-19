ffe6cbef627fec160b2553f7effb90ef84de4829
DmitryRedka <dmredka@gmail.com>
2023-02-19 19:12:56 +0300

    add web view

--- Image Feed/Auth/AuthViewController.swift
+++ Image Feed/Auth/AuthViewController.swift
@@ -3,4 +3,27 @@ import UIKit
 final class AuthViewController: UIViewController {
     private let segueWebViewIdentifier = "ShowWebView"
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == segueWebViewIdentifier {
+            guard let webViewViewController = segue.destination as? WebViewViewController else {
+                fatalError("Failed to prepare for \(segueWebViewIdentifier)")
+            }
+            webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+}
+
+extension AuthViewController: WebViewViewControllerDelegateProtocol {
+    
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        // TODO code
+    }
+    
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        dismiss(animated: true)
+    }
+    
+    
 }
